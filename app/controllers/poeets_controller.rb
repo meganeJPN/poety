6159742d80fc74b128d51bb0e43b37ef5a4694bf
meganeJPN <ooosamuuu1985@gmail.com>
2020-06-16 15:35:55 +0900

    一覧画面と入力画面を作成

--- app/controllers/poeets_controller.rb
+++ app/controllers/poeets_controller.rb
@@ -1,4 +1,20 @@
 class PoeetsController < ApplicationController
   def index
+    @poeets = Poeet.all
+  end
+
+  def new
+    @poeet = Poeet.new
+  end
+
+  def create
+    # Poeet.create(content: params[:poeet][:content])
+    Poeet.create(poeet_params)
+    redirect_to poeets_path
+  end
+
+  private
+  def poeet_params
+    params.require(:poeet).permit(:content)
   end
 end
